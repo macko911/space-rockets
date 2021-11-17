4e0c2b336fb6b2bca6fc3adf86a05aef97f7bac1
Matej Vobornik <vobornik.matej@gmail.com>
2021-11-17 23:46:14 +0100

    Refactor FavouriteButton

--- src/components/favourite-launch-button.js
+++ src/components/favourite-launch-button.js
@@ -1,23 +1,14 @@
 import React from "react";
-import { Star } from "react-feather";
-import { IconButton } from "@chakra-ui/core";
 import { useFavouriteLaunch } from "../utils/favourites-context";
+import FavouriteButton from "./favourite-button";
 
 export default function FavouriteLaunchButton({ launchId }) {
   const { isFavourite, toggleFavouriteLaunch } = useFavouriteLaunch(launchId);
 
-  function handleFavouriteClick(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    toggleFavouriteLaunch();
-  }
-
   return (
-    <IconButton
-      aria-label="Favourite button"
-      variantColor={isFavourite ? "teal" : undefined}
-      icon={Star}
-      onClick={handleFavouriteClick}
+    <FavouriteButton
+      isFavourite={isFavourite}
+      onClick={toggleFavouriteLaunch}
     />
   );
 }
--- src/components/favourite-launch-pad-button.js
+++ src/components/favourite-launch-pad-button.js
@@ -1,23 +1,14 @@
 import React from "react";
-import { Star } from "react-feather";
-import { IconButton } from "@chakra-ui/core";
 import { useFavouriteLaunchPad } from "../utils/favourites-context";
+import FavouriteButton from "./favourite-button";
 
 export default function FavouriteLaunchPadButton({ launchPadId }) {
   const { isFavourite, toggleFavouriteLaunchPad } = useFavouriteLaunchPad(launchPadId);
 
-  function handleFavouriteClick(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    toggleFavouriteLaunchPad();
-  }
-
   return (
-    <IconButton
-      aria-label="Favourite button"
-      variantColor={isFavourite ? "teal" : undefined}
-      icon={Star}
-      onClick={handleFavouriteClick}
+    <FavouriteButton
+      isFavourite={isFavourite}
+      onClick={toggleFavouriteLaunchPad}
     />
   );
 }
