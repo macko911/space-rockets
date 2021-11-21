caf1255e49eec085a850767a8b80aa4d3d3b4076
Matej Vobornik <vobornik.matej@gmail.com>
2021-11-21 21:15:09 +0100

    Setup Cypress tests

---
+++ cypress/plugins/index.js
@@ -0,0 +1,11 @@
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+// You can read more here: https://on.cypress.io/plugins-guide
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+};
